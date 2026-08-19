848e30a19d0218ea363b3a320c97291733c57e1b
Levi Zitting <me@levizitting.com>
2026-08-18 19:41:12 -0500

    feat: add existing members

---
+++ src/tf/members.tf
@@ -0,0 +1,64 @@
+resource "github_membership" "aaron_merrick" {
+  username = "Aaron-Merrick"
+  role     = "member"
+}
+
+resource "github_membership" "burnettenguyen" {
+  username = "burnettenguyen"
+  role     = "member"
+}
+
+resource "github_membership" "fredlawl" {
+  username = "fredlawl"
+  role     = "member"
+}
+
+resource "github_membership" "glitchedmob" {
+  username = "glitchedmob"
+  role     = "admin"
+}
+
+resource "github_membership" "jason_klein" {
+  username = "jason-klein"
+  role     = "member"
+}
+
+resource "github_membership" "jugglingdev" {
+  username = "jugglingdev"
+  role     = "admin"
+}
+
+resource "github_membership" "motter360" {
+  username = "Motter360"
+  role     = "member"
+}
+
+resource "github_membership" "nomedis" {
+  username = "nomedis"
+  role     = "member"
+}
+
+resource "github_membership" "ntoombs19" {
+  username = "ntoombs19"
+  role     = "member"
+}
+
+resource "github_membership" "opensgf" {
+  username = "opensgf"
+  role     = "admin"
+}
+
+resource "github_membership" "sm0862512" {
+  username = "sm0862512"
+  role     = "member"
+}
+
+resource "github_membership" "thing_engineer" {
+  username = "ThingEngineer"
+  role     = "member"
+}
+
+resource "github_membership" "zack_bradshaw" {
+  username = "ZackBradshaw"
+  role     = "member"
+}
